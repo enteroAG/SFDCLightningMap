d50581224b2c81fbde8942eebb568deec7d244a6
fabian384 <Fabian.peters@entero.de>
2018-05-25 17:06:42 +0200

    Add files via upload

--- src/aura/AccountMap/AccountMapController.js
+++ src/aura/AccountMap/AccountMapController.js
@@ -1,35 +1,55 @@
 ({
     jsLoaded: function(component, event, helper) {
-		// now re-create your map with markers
-		var mapDiv = component.find('mapid').getElement();
-        var map = L.map(mapDiv, {zoomControl: true, tap: false, zoomAnimation:true,fadeAnimation:true,markerZoomAnimation:true}).setView([50.256737697601494, 7.648483591683552], 14);
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
-            {
-                attribution: 'Tiles © Esri'
-            }).addTo(map);
-        component.set("v.map", map);
+        try {
+            // now re-create your map with markers
+            var mapDiv = component.find('mapid').getElement();
+            var map = L.map(mapDiv, {zoomControl: true, tap: false, zoomAnimation:false,fadeAnimation:true,markerZoomAnimation:true}).setView([50.256737697601494, 7.648483591683552], 14);
+            L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+                {
+                    attribution: 'Tiles © Esri'
+                }).addTo(map);
+            component.set("v.map", map);
+        } catch (e) {
+            // Handle error
+            console.error(e);
+            helper.showWarningToast(e.message);
+        }
     },
     accountsLoaded: function(component, event, helper) {
-        // Add markers
-        var map = component.get('v.map');
-        var accounts = event.getParam('accounts');
-        for (var i=0; i<accounts.length; i++) {
-            var account = accounts[i];
-            var latLng = [account.BillingLatitude, account.BillingLongitude];
-            L.marker(latLng, {account: account, title: account.Name}).addTo(map).on('click', function(event) {
-    			helper.navigateToDetailsView(event.target.options.account.Id);
-            });
-        }  
-        
-        // center map to first account
-        if(accounts[0]){
-            map.panTo([accounts[0].BillingLatitude, accounts[0].BillingLongitude]);
+        try {
+            // Add markers
+            var map = component.get('v.map');
+            var accounts = event.getParam('accounts');
+            for (var i=0; i<accounts.length; i++) {
+                var account = accounts[i];
+                var latLng = [account.BillingLatitude, account.BillingLongitude];
+                var marker = L.marker(latLng, {account: account});
+                //marker.bindTooltip(account.Name).openTooltip();
+                marker.addTo(map).bindPopup(account.Name, {autoClose:false}).openPopup();
+                marker.addTo(map).on('click', function(event) {
+                    helper.navigateToDetailsView(event.target.options.account.Id);
+                });
+            }
+            // center map to first account
+            if(accounts[0]){
+                map.panTo([accounts[0].BillingLatitude, accounts[0].BillingLongitude]);
+            }
+        } catch (e) {
+            // Handle error
+            console.error(e);
+            helper.showWarningToast(e.message);
         }
     },
     accountSelected: function(component, event, helper) {
         // Center the map on the account selected in the list
-        var map = component.get('v.map');
-        var account = event.getParam("account");
-        map.panTo([account.BillingLatitude, account.BillingLongitude]);
+        try {
+            var map = component.get('v.map');
+            var account = event.getParam("account");
+            map.panTo([account.BillingLatitude, account.BillingLongitude]);
+        } catch (e) {
+            // Handle error
+            console.error(e);
+            helper.showWarningToast(e.message);
+        }
     }
 })
--- src/aura/AccountMap/AccountMapHelper.js
+++ src/aura/AccountMap/AccountMapHelper.js
@@ -5,5 +5,18 @@
             "recordId": accountId
         });
         event.fire();
-    }
+    },
+    
+    showWarningToast : function(message) {
+    var toastEvent = $A.get("e.force:showToast");
+    
+    toastEvent.setParams({
+        "title": "Warning",
+        "message":"an error occured",
+        "messageTemplate": "Please contact your admin (\"{0}\")",
+        "messageTemplateData": [message],
+        "type": "warning"
+    });
+    toastEvent.fire();
+}
 })
